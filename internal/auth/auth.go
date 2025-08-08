e7ba57b9efa878db47c49a84c2bbec0caaec8da4
CzarRamos <czar.ramos.co@gmail.com>
2025-08-07 18:08:06 -0700

    Updated password hashing unit tests and added JWT token creation and validation system

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,25 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
+	if len(password) <= 0 {
+		return "", errors.New("error: password cannot be empty")
+	}
+
+	// 72 bytes is the limit for our password
+	// Two different passwords with the same first 72 bytes will be considered the same password
+	if len(password) > 72 {
+		return "", errors.New("error: password is too long")
+	}
 
 	newHash, err := bcrypt.GenerateFromPassword([]byte(password), 4)
 	if err != nil {
@@ -14,4 +31,42 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	currentTime := time.Now()
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(currentTime),
+		ExpiresAt: jwt.NewNumericDate(currentTime.Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signedString, err := newToken.SignedString([]byte(tokenSecret))
+	if err != nil {
+		log.Printf("error creating signed string: %s", err)
+		return "", err
+	}
+
+	return signedString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		log.Printf("error validating token: %s", err)
+		return uuid.Nil, err
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		log.Printf("error parsing user uuid: %s", err)
+		return uuid.Nil, err
+	}
+
+	return userID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,25 +1,185 @@
 package auth_test
 
 import (
-	"strings"
 	"testing"
+	"time"
 
 	"github.com/CzarRamos/chirpy/internal/auth"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
 )
 
-func TestPasswordHashing(t *testing.T) {
-	rawPassword := "go@Testing!2453"
-	want, err := bcrypt.GenerateFromPassword([]byte(rawPassword), 4)
+func TestGeneralPasswordHashing(t *testing.T) {
+	originalPassword := "my-super-secure-password"
+
+	output, err := auth.HashPassword(originalPassword)
 	if err != nil {
-		t.Errorf(`bcrypt.GenerateFromPassword = %v`, err)
+		t.Errorf(`error hashing password did not work: %v`, err)
+		return
 	}
-	output, err := auth.HashPassword(rawPassword)
+
+	err = auth.CheckPasswordHash(originalPassword, output)
 	if err != nil {
-		t.Errorf(`bcrypt.GenerateFromPassword = %v`, err)
+		t.Errorf(`CheckPasswordHash failed with the correct password: %v`, err)
+		return
 	}
-	isMatch := strings.Compare(string(want), string(output))
-	if isMatch > 0 {
-		t.Errorf(`bcrypt.GenerateFromPassword = %q, %v, want match for %#q, nil`, output, err, want)
+
+	err = auth.CheckPasswordHash("some-random-password", output)
+	if err == nil {
+		t.Errorf(`CheckPasswordHash should have failed with the incorrect password: %v`, err)
+		return
+	}
+}
+
+func TestPasswordHashingTwice(t *testing.T) {
+	originalPassword := "my-super-secure-password"
+
+	// same password, just hashed at different moments
+	hash1, err := auth.HashPassword(originalPassword)
+	if err != nil {
+		t.Errorf(`error hashing password did not work: %v`, err)
+		return
+	}
+
+	hash2, err := auth.HashPassword(originalPassword)
+	if err != nil {
+		t.Errorf(`error hashing password did not work: %v`, err)
+		return
+	}
+
+	// both hashes should work
+	err = auth.CheckPasswordHash(originalPassword, hash1)
+	if err != nil {
+		t.Errorf(`CheckPasswordHash failed with the first hash verison of the password: %v`, err)
+		return
+	}
+
+	err = auth.CheckPasswordHash("some-random-password", hash2)
+	if err == nil {
+		t.Errorf(`CheckPasswordHash failed with a second hash verison of the password %v`, err)
+		return
+	}
+}
+
+func TestInvalidPasswordLengthHashing(t *testing.T) {
+	emptyPassword := ""
+	veryLongPassword := "A9v!X2blrmZP0qsd4eT7fyUjKDfghjQWERTYUIOPasdfghjklzxcvbnm1234567890!@#$%^&*()_+QWERTY"
+
+	// same password, just hashed at different moments
+	_, err := auth.HashPassword(emptyPassword)
+	if err == nil {
+		t.Errorf(`HashPassword should not have succeeded hashing an empty password: %v`, err)
+		return
+	}
+
+	_, err = auth.HashPassword(veryLongPassword)
+	if err == nil {
+		t.Errorf(`HashPassword should not have succeeded hashing a password over 72 bytes: %v`, err)
+		return
+	}
+}
+
+func TestJWTValidToken(t *testing.T) {
+	//generate uuid
+	userID := uuid.New()
+	//token secret
+	tokenSecret := "this-is-my-secret-token"
+	//duration
+	duration := time.Minute
+	newJWT, err := auth.MakeJWT(userID, tokenSecret, duration)
+	if err != nil {
+		t.Errorf(`MakeJWT failed: %v`, err)
+		return
+	}
+
+	output, err := auth.ValidateJWT(newJWT, tokenSecret)
+	if err != nil {
+		t.Errorf(`ValidateJWT failed: %v`, err)
+		return
+	}
+
+	if output != userID {
+		t.Errorf(`ValidateJWT returned wrong userID: got %v, want %v`, output, userID.String())
+		return
+	}
+}
+
+func TestExpiredTokens(t *testing.T) {
+	//generate uuid
+	userID := uuid.New()
+	//token secret
+	tokenSecret := "this-is-my-secret-token"
+	//duration
+	duration := -time.Hour
+	newJWT, err := auth.MakeJWT(userID, tokenSecret, duration)
+	if err != nil {
+		t.Errorf(`MakeJWT failed: %v`, err)
+		return
+	}
+
+	_, err = auth.ValidateJWT(newJWT, tokenSecret)
+	if err == nil {
+		t.Errorf(`ValidateJWT should not have accepted expired token`)
+	}
+}
+
+func TestWrongSecretKey(t *testing.T) {
+	//generate uuid
+	userID := uuid.New()
+	//token secret
+	tokenSecret := "this-is-my-secret-token"
+	// some token secret for something else
+	differentTokenSecret := "this-is-a-different-secret-token"
+	//duration
+	duration := time.Minute
+	newJWT, err := auth.MakeJWT(userID, tokenSecret, duration)
+	if err != nil {
+		t.Errorf(`MakeJWT failed: %v`, err)
+		return
+	}
+
+	_, err = auth.ValidateJWT(newJWT, differentTokenSecret)
+	if err == nil {
+		t.Errorf(`ValidateJWT should have rejected when validating with a different key than initialization`)
+	}
+}
+
+func TestValidatingInvalidStrings(t *testing.T) {
+
+	//token secret
+	tokenSecret := "this-is-my-secret-token"
+
+	_, err := auth.ValidateJWT("a-made-up-token", tokenSecret)
+	if err == nil {
+		t.Errorf(`ValidateJWT should have rejected non-existent token`)
+	}
+
+	// test with empty string
+	_, err = auth.ValidateJWT("", tokenSecret)
+	if err == nil {
+		t.Errorf(`ValidateJWT should have rejected validating an empty string`)
+	}
+}
+
+func TestCorruptedToken(t *testing.T) {
+
+	//generate uuid
+	userID := uuid.New()
+	//token secret
+	tokenSecret := "this-is-my-secret-token"
+	//duration
+	duration := time.Minute
+	newJWT, err := auth.MakeJWT(userID, tokenSecret, duration)
+	if err != nil {
+		t.Errorf(`MakeJWT failed: %v`, err)
+		return
+	}
+
+	// corruption happens
+	corruptedJWT := newJWT + "i-am-inflitrating-the-token"
+
+	_, err = auth.ValidateJWT(corruptedJWT, tokenSecret)
+	if err == nil {
+		t.Errorf(`ValidateJWT should have rejected a corrupted/modified token`)
+		return
 	}
 }
